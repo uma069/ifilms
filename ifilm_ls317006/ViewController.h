9155837d23146d9dcc2e2f8d1ee708594f587ade
Alumne <alumne@imac-de-lasallebcn.local>
2018-03-12 17:32:10 +0100

    Initial Commit

---
+++ ifilm_ls317006/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ifilm_ls317006
+//
+//  Created by Alumne on 12/3/18.
+//  Copyright © 2018 Belowapiti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ifilm_ls317006/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ifilm_ls317006
+//
+//  Created by Alumne on 12/3/18.
+//  Copyright © 2018 Belowapiti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
